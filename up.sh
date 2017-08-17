b75fed31f98dd01d616880eac76a5565cf720fa8
Ryuji <taku.kisi.1989@gmail.com>
2017-08-18 07:11:31 +0900

    [update]環境構築に伴う最終調整

--- up.sh
+++ up.sh
@@ -9,24 +9,22 @@ fi
 
 if [ $1 = "local" ]; then
   ps aux | grep lens-manage | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-  nohup rails s -b 0.0.0.0 &
+  nohup bundle exec rails s -b 0.0.0.0 &
 
   echo "== open as local =="
   echo "http://$IP_ADD:3000/"
 elif [ $1 = "development" ]; then
   ps aux | grep lens-manage | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-  nohup puma -w 4 &
+  nohup bundle exec puma -w 4 &
 
   echo "== open as development =="
 elif [ $1 = "production" ]; then
   cat /home/app/run/lens-manage.pid | awk '{ print "kill -9", $0 }' | sh
-  rails assets:precompile RAILS_ENV=production
-  SECRET_KEY_BASE=`rails secret` RAILS_SERVE_STATIC_FILES=true RAILS_ENV=production nohup puma -w 4 &
-  #RAILS_SERVE_STATIC_FILES=true RAILS_ENV=production puma -w 4 &
+  bundle exec rails assets:precompile RAILS_ENV=production
+  SECRET_KEY_BASE=`bundle exec rails secret` RAILS_SERVE_STATIC_FILES=true RAILS_ENV=production nohup bundle exec puma -w 4 &
 
   echo "== open as production =="
 else
   echo "有効な引数ではありません。local, development, productionを指定して下さい"
   exit 1
 fi
-
