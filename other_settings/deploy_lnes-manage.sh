e8f50c287ef19e608e63e75b4a3e2012c82bb32a
Ryuji <taku.kisi.1989@gmail.com>
2017-08-27 20:24:41 +0900

    [update]デプロイ設定を修正

---
+++ other_settings/deploy_lnes-manage.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# デプロイ用シェル
+# /home/app
+
+PROJECT_NAME=lens-manage
+
+if [ $# -ne 1 ]; then
+  echo "引数にinit, app, manageを指定して下さい"
+  exit 1
+fi
+
+if [ $1 = "init" ]; then
+  git clone https://github.com/kanagawa41/$PROJECT_NAME.git
+  cd $PROJECT_NAME
+  bundle install --path vendor/bundle
+  cd ../
+  mkdir -p vendors/$PROJECT_NAME
+  cp -r lens-manage/vendor/bundle vendors/$PROJECT_NAME
+elif [ $1 = "app" ]; then
+  rm -rf $PROJECT_NAME
+  git clone https://github.com/kanagawa41/$PROJECT_NAME.git
+  cp -r vendors/$PROJECT_NAME/bundle lens-manage/vendor
+elif [ $1 = "manage" ]; then
+  rm -rf $PROJECT_NAME
+  git clone https://github.com/kanagawa41/$PROJECT_NAME.git
+  cp -r vendors/$PROJECT_NAME/bundle lens-manage/vendor
+else
+  echo "有効な引数ではありません。init, app, manageを指定して下さい"
+  exit 1
+fi
