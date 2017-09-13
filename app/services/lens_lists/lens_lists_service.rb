d01b0b0c35f67b5c7fd70b3330b1fc9c0b8c0243
Ryuji <taku.kisi.1989@gmail.com>
2017-09-14 01:17:30 +0900

    [update]検索の順序を作成昇順に修正

--- app/services/lens_lists/lens_lists_service.rb
+++ app/services/lens_lists/lens_lists_service.rb
@@ -1,10 +1,10 @@
 class LensListsService  
   def self.top
-    MLensInfo.where.not(lens_info_url: nil).order(:created_at).limit(12).all
+    MLensInfo.where.not(lens_info_url: nil).order(created_at: :desc).limit(12).all
   end
 
   def self.index(query, page=1)
-    MLensInfo.where(disabled: false).where("lens_name like ?", "%" + query + "%").order(:created_at).page(page)
+    MLensInfo.where(disabled: false).where("lens_name like ?", "%" + query + "%").order(created_at: :desc).page(page)
   end
 
 end
