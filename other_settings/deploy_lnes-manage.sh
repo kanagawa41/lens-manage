7882c41785c031bb5668529ae63114eb438b46b2
Ryuji <taku.kisi.1989@gmail.com>
2017-09-24 12:46:27 +0900

    [update]デプロイシェルの作成

--- other_settings/deploy_lnes-manage.sh
+++ other_settings/deploy_lnes-manage.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+####################
+# about
+####################
 # Railsプロジェクトのデプロイ用シェル
 # 対象プロジェクトの一階層に設置する。
 # 例)
@@ -8,46 +11,69 @@
 # 引数)
 # 第１：処理内容、第２：プロジェクト名(任意)
 
-echo "== Start deployment! =="
+####################
+# base setting
+####################
+# 実行対象（順番は重要）
+declare -A TYPES=([app]="lens-manage" [manage]="m-lens-manage")
+# GITプロジェクト名
+GIT_PROJECT_URL=https://github.com/kanagawa41/lens-manage.git
+
+####################
+# usage
+####################
+cmdname=`basename $0`
+function usage()
+{
+  type_str="$(IFS=,; echo "${PROJECTS[*]}")"
+  echo "Usage: ${cmdname} [OPTIONS] TYPE"
+  echo "  This script is ~."
+  echo "Options:"
+  echo "  -i, initialize"
+  echo "TYPE:"
+  echo "  using ${type_str}"
+  exit 1
+}
+
+####################
+# Parameter check
+####################
+declare -i argc=0
+declare -a argv=()
+while (( $# > 0 ))
+do
+    case "$1" in
+        -*)
+            if [[ "$1" =~ '-i' ]] || [[ "$1" =~ '--init' ]]; then
+                i_flag='TRUE'
+            fi
+            shift
+            ;;
+        *)
+            ((++argc))
+            argv=("${argv[@]}" "$1")
+            shift
+            ;;
+    esac
+done
 
 ####################
 # Parameter varidations
 ####################
-valid_params () {
-  # 引数に配列を受け取るためのおまじない
-  local param_1=$1
-  shift
-  local param_2=($@)
-
-  # if文に[]をつけると意図しない動きになる
-  if ! `echo ${param_2[@]} | grep -qw "$param_1"` ; then
-    delimiter=", "; str=""; for var in ${param_2[@]}; do str+="${delimiter}'${var}'"; done; str=`echo $str | sed -e "s/^${delimiter}//"`
-    echo "有効な引数ではありません。$strから指定して下さい。"
-    exit 1
-  fi
-}
-
-# 実行対象（順番は重要）
-DEPS=("init" "app" "manage")
-# プロジェクト名（順番は重要）
-PROJECTS=("lens-manage" "m-lens-manage")
-if [ $# -ne 2 ]; then
-  echo "引数に実行タイプ(${DEPS[@]}), プロジェクト名(${PROJECTS[@]})を渡してください。"
+target_type=${argv[0]}
+target_project=${TYPES[${argv[0]}]}
+if [ "${target_project}" = "" ]; then
+  usage
   exit 1
 fi
-
-valid_params $1 ${DEPS[@]}
-valid_params $2 ${PROJECTS[@]}
 
 ####################
 # Variables
 ####################
 # シェルパス
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-# GITプロジェクト名
-GIT_PROJECT_URL=https://github.com/kanagawa41/lens-manage.git
 # プロジェクト名
-PROJECT_NAME=$2
+PROJECT_NAME=$target_project
 
 # プロジェクト絶対パス
 PROJECT_PATH=$SCRIPT_DIR/$PROJECT_NAME
@@ -76,7 +102,9 @@ ifrm () {
 ####################
 # Main
 ####################
-if [ $1 = ${DEPS[0]} ]; then
+echo "== Start deployment! =="
+
+if [ "${i_flag}" = "TRUE" ]; then
   ifrm $PROJECT_PATH
   ifrm $BK_PROJECT_PATH
   git clone $GIT_PROJECT_URL $PROJECT_NAME
@@ -88,14 +116,15 @@ if [ $1 = ${DEPS[0]} ]; then
 
   mkdir -p $BK_LOG
 
-elif [ $1 = ${DEPS[1]} ] || [ $1 = ${DEPS[2]} ]; then
+else
   cp -r $ORG_LOG $BK_LOG
   rm -rf $PROJECT_PATH
   git clone $GIT_PROJECT_URL $PROJECT_NAME
 
-  if [ $1 = ${DEPS[1]} ]; then
+  if [ "${target_type}" = "app" ]; then
     # routesを修正
     cp -f $PROJECT_PATH/config/routes-app.rb $PROJECT_PATH/config/routes.rb
+    echo "== Changed routes.rb for app! =="
   fi
 
   # バックアップを移行する
@@ -114,11 +143,12 @@ elif [ $1 = ${DEPS[1]} ] || [ $1 = ${DEPS[2]} ]; then
 
   cd $PROJECT_PATH
 
-  if [ $1 = ${DEPS[1]} ]; then
+  if [ "${target_type}" = "app" ]; then
     ./up.sh production
   else
     ./up.sh production 3002
   fi
+
 fi
 
-echo "== Success! =="
+echo "== End deployment with success! =="
--- up.sh
+++ up.sh
@@ -64,7 +64,11 @@ if [ $1 = ${ENVS[0]} ]; then
   if [ ! $? = 0 ] ; then echo "FAILD: set up rails."; exit 1; fi
 
   SELF_IP=`hostname -I | cut -f2 -d' '` #自身のＩＰを取得
-  echo "http://$SELF_IP:$PORT/"
+  if [ -n "$PORT" ]; then
+    echo "http://$SELF_IP:$PORT/"
+  else
+    echo "http://$SELF_IP:3000/"
+  fi
   echo "== Stand up as ${ENVS[0]} =="
 elif [ $1 = ${ENVS[1]} ]; then
   ps aux | grep -w "\[$PROJECT_NAME\]" | grep -v grep | awk '{ print "kill -9", $2 }' | sh
@@ -78,6 +82,7 @@ elif [ $1 = ${ENVS[2]} ]; then
   bundle exec rails assets:clean RAILS_ENV=production # クリーンしても直近３バージョンは保持される
   bundle exec rails assets:precompile RAILS_ENV=production
   RAILS_ENV=production bundle exec whenever --update-crontab
+  # ポートを指定するとsockが使用できない
   SECRET_KEY_BASE=`bundle exec rails secret` RAILS_SERVE_STATIC_FILES=true RAILS_ENV=production nohup bundle exec puma -w 4 $PORT &
 
   if [ ! $? = 0 ] ; then echo "FAILD: set up rails."; exit 1; fi
