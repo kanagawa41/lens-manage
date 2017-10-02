967a5455ed3a4909fff40cde7047149959503fbd
Ryuji <taku.kisi.1989@gmail.com>
2017-10-03 02:20:59 +0900

    [update]最新表示の場合、フラグを保持しないようにリダイレクトするようにした

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,12 +5,15 @@ class AdminController < ApplicationController
   # https://github.com/ruby-openstack/ruby-openstack/wiki/Object-Storage
   def conoha_list
     @js_tree, @container_metadata, @file_stamp = AdminService::conoha_list(params[:force_fetch_flag])
+    if params[:force_fetch_flag]
+      redirect_to action: "conoha_list"
+    end
   end
 
   # TODO: ajax通信で実行できるようにする
   def delete_objects
   end
-  
+
   private
 
 end
