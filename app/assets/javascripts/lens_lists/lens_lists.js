1b4789a6fe888a9c4a447cce32211efe05316932
Ryuji <taku.kisi.1989@gmail.com>
2017-10-08 17:12:09 +0900

    [update]検索画面

--- app/assets/javascripts/lens_lists/lens_lists.js
+++ app/assets/javascripts/lens_lists/lens_lists.js
@@ -6,6 +6,20 @@ modules.lensLists = (function () {
 var module = {}
 
 module.init = function() {
+  $('#detail_search').on('click', function(){
+    $('#modal_box').modal('show');
+  });
+
+  // 詳細で検索
+  $('#detail_search_btn').on('click', function(){
+    $('#search_exec').click();
+  });
+
+  // 条件のリセット
+  $('#detail_reset_btn').on('click', function(){
+    $('#focal_length').val('');
+    $('#f_num').val('');
+  });
 }
 
 return module;
