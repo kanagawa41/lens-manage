41d1688039f04bc384ecdabf4f8374730d59bf13
kishiratakuya <kishiratakuya@team-lab.com>
2017-10-18 16:57:42 +0900

    [update]新規テーブルにユニーク制約を付与(追加)

--- db/migrate/20171018070005_add_uniq_of_m_lens_info_id_to_analytics_lens_infos.rb
+++ db/migrate/20171018070005_add_uniq_of_m_lens_info_id_to_analytics_lens_infos.rb
@@ -1,5 +1,8 @@
 class AddUniqOfMLensInfoIdToAnalyticsLensInfos < ActiveRecord::Migration[5.1]
   def change
+  	remove_foreign_key :analytics_lens_infos, :m_lens_infos
+  	remove_index :analytics_lens_infos, :m_lens_info_id
   	add_index :analytics_lens_infos, :m_lens_info_id, unique: true
+  	add_foreign_key :analytics_lens_infos, :m_lens_infos
   end
 end
